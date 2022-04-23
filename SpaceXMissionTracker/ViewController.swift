35d3882e7fa8fcbaa04589626c7a293b6b911536
Anthony Glyadchenko <aglyad@gmail.com>
2022-04-23 18:07:04 -0400

    Initial generated project

---
+++ SpaceXMissionTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceXMissionTracker
+//
+//  Created by Anthony Glyadchenko on 4/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
