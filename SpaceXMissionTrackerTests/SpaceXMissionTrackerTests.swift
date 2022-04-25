92d0aa5cc6024530eed86a9ca0ea5e085029074d
Anthony Glyadchenko <aglyad@gmail.com>
2022-04-25 00:20:04 -0400

    Add sort unit tests for MissionListViewModelImpl

--- SpaceXMissionTrackerTests/SpaceXMissionTrackerTests.swift
+++ SpaceXMissionTrackerTests/SpaceXMissionTrackerTests.swift
@@ -1,36 +1,49 @@
-//
-//  SpaceXMissionTrackerTests.swift
-//  SpaceXMissionTrackerTests
-//
-//  Created by Anthony Glyadchenko on 4/23/22.
-//
-
 import XCTest
 @testable import SpaceXMissionTracker
 
 class SpaceXMissionTrackerTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    var viewModel: MissionsListViewModelImpl!
+    
+    func testWhenMissionsListByRocketNameSortIsPassedToServiceThatItIsMappedToRocketName() {
+        //given
+        let asyncExpectation = expectation(description: "Service call finishes")
+        let mockMissionListService = MockMissionListService()
+        viewModel = MissionsListViewModelImpl(missionListService: mockMissionListService)
+        
+        //when
+        viewModel.listOfSpaceXMissions(sort: .byRocketName) { _ in
+            asyncExpectation.fulfill()
         }
+        
+        //then
+        waitForExpectations(timeout: 1000)
+        XCTAssertEqual(mockMissionListService.savedSort, MissionListServiceSortOptions.rocketName)
     }
+    
+    func testWhenMissionsListYearSortIsPassedToServiceThatItIsMappedToYear() {
+        //given
+        let asyncExpectation = expectation(description: "Service call finishes")
+        let mockMissionListService = MockMissionListService()
+        viewModel = MissionsListViewModelImpl(missionListService: mockMissionListService)
+        
+        //when
+        viewModel.listOfSpaceXMissions(sort: .byYear) { _ in
+            asyncExpectation.fulfill()
+        }
+        
+        //then
+        waitForExpectations(timeout: 1000)
+        XCTAssertEqual(mockMissionListService.savedSort, MissionListServiceSortOptions.year)
+    }
+    override func tearDownWithError() throws {
+        viewModel = nil
+    }
+}
 
+class MockMissionListService: MissionListService {
+    var savedSort: MissionListServiceSortOptions!
+    func fetchMissions(filter: MissionListServiceFilterOptions, sort: MissionListServiceSortOptions, limit: UInt, completion: @escaping (Response<MissionsResponse>) -> Void) {
+        savedSort = sort
+        completion(.Data(MissionsResponse(missions: [])))
+    }
 }
